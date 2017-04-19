763c8638d0b818a500aeb9b3e854ee1cbdf2eadf
paolo <antonio.mateo@47deg.com>
2017-04-19 14:08:09 +0200

    Solves comment "Encoder"

--- src/main/scala/circelib/EncodingDecodingSection.scala
+++ src/main/scala/circelib/EncodingDecodingSection.scala
@@ -20,7 +20,7 @@ object EncodingDecodingSection
   import circelib.helpers.EncodingHelpers._
 
   /**
-   * Circe uses `Encoder` and `Decoder` type classes for encoding and decoding. An `Ecoder[A]` instance provides a function
+   * Circe uses `Encoder` and `Decoder` type classes for encoding and decoding. An `Encoder[A]` instance provides a function
    * that will convert any `A` to a `Json` and a `Decoder[A]` takes a `Json` value to either an exception or an `A`.circe provides
    * implicit instances of these type classes for many types from the Scala standard library, including `Int`, `String`, and others.
    * It also provides instances for `List[A]`, `Option[A]`, and other generic types, but only if `A` has an `Encoder` instance.
