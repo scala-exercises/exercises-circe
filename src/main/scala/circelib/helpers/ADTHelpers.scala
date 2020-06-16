624c540fc810e6a1c6c7d83b831ab2824ed2829a
Ben Fradet <benjamin.fradet@gmail.com>
2020-06-16 11:21:06 +0200

    Update scalafmt-core to 2.5.3

--- src/main/scala/circelib/helpers/ADTHelpers.scala
+++ src/main/scala/circelib/helpers/ADTHelpers.scala
@@ -49,14 +49,12 @@ object ADTHelpers {
   object ShapesDerivation {
     import shapeless.{Coproduct, Generic}
 
-    implicit def encodeAdtNoDiscr[A, Repr <: Coproduct](
-        implicit
+    implicit def encodeAdtNoDiscr[A, Repr <: Coproduct](implicit
         gen: Generic.Aux[A, Repr],
         encodeRepr: Encoder[Repr]
     ): Encoder[A] = encodeRepr.contramap(gen.to)
 
-    implicit def decodeAdtNoDiscr[A, Repr <: Coproduct](
-        implicit
+    implicit def decodeAdtNoDiscr[A, Repr <: Coproduct](implicit
         gen: Generic.Aux[A, Repr],
         decodeRepr: Decoder[Repr]
     ): Decoder[A] = decodeRepr.map(gen.from)
